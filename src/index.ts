26a0f768727c88835e4e1b09257fcd54a3fa8707
Richard <richerd.rdc@gmail.com>
2023-04-21 18:39:08 -0700

    Export more utils

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ export * from "@rbxts/flipper";
 
 export * from "./utils/hoarcekat";
 export * from "./utils/testez";
+export * from "./utils/motor";
+export * from "./utils/binding";
 
 export * from "./use-camera";
 export * from "./use-previous";
