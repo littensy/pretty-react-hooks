6a19d27416db5e0f21e335dc59f79488656a2d72
Richard <richerd.rdc@gmail.com>
2023-04-21 20:23:20 -0700

    Update use-memoized-callback.ts

--- src/use-memoized-callback/use-memoized-callback.ts
+++ src/use-memoized-callback/use-memoized-callback.ts
@@ -1,11 +1,16 @@
 import { useCallback, useMutable } from "@rbxts/roact-hooked";
 
-export function useMemoizedCallback<T extends Callback>(callback: T): T;
-export function useMemoizedCallback(callback: Callback): Callback {
+/**
+ * Returns a memoized callback. When passed a new callback, the memoized
+ * callback will not change, but calling it will invoke the new callback.
+ * @param callback The callback to memoize.
+ * @returns The memoized callback.
+ */
+export function useMemoizedCallback<T extends Callback>(callback: T): T {
 	const callbackRef = useMutable(callback);
 	callbackRef.current = callback;
 
-	return useCallback((...args) => {
+	return useCallback((...args: unknown[]) => {
 		return callbackRef.current(...args);
-	}, []);
+	}, []) as T;
 }
