c15502fdbdbd7620e2cbaf146c8107b20f862194
Richard <richerd.rdc@gmail.com>
2023-06-05 01:31:10 -0700

    feat: add hook detection options parameter

--- src/utils/hoarcekat.tsx
+++ src/utils/hoarcekat.tsx
@@ -1,16 +1,17 @@
 import Roact from "@rbxts/roact";
-import { withHookDetection } from "@rbxts/roact-hooked";
+import { HookDetectionOptions, withHookDetection } from "@rbxts/roact-hooked";
 
 /**
  * Returns a function that can be used as a Hoarcekat story. This function will
  * mount the given component to the target instance and unmount it when the
  * story is unmounted.
  * @param TestComponent The component to mount.
+ * @param options Optional options to pass to `withHookDetection`.
  * @returns A Hoarcekat story.
  */
-export function hoarcekat(TestComponent: Roact.FunctionComponent) {
+export function hoarcekat(TestComponent: Roact.FunctionComponent, options?: HookDetectionOptions) {
 	return (target: Instance) => {
-		withHookDetection(Roact);
+		withHookDetection(Roact, options);
 
 		const handle = Roact.mount(<TestComponent />, target);
 
