1876a976e08f749b1fed8c495d9a6abe5121a3aa
g1mmethemoney <theguywholovecheetos@gmail.com>
2024-04-13 23:36:15 +0300

    fix: import the type directly

--- src/use-mount-effect/use-mount-effect.ts
+++ src/use-mount-effect/use-mount-effect.ts
@@ -1,9 +1,9 @@
-import React, { useEffect } from "@rbxts/react";
+import { EffectCallback, useEffect } from "@rbxts/react";
 
 /**
  * Runs a callback when the component is mounted.
  * @param callback The callback to run.
  */
-export function useMountEffect(callback: React.EffectCallback) {
+export function useMountEffect(callback: EffectCallback) {
 	useEffect(callback, []);
 }
